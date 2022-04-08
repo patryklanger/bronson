78cf586783c532975a5fa64f9c58b220912ef494
Patryk Langer <patryklanger@icloud.com>
2022-04-08 12:23:32 +0200

    .

--- src/app/pages/contact-form/contact-form.component.ts
+++ src/app/pages/contact-form/contact-form.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { getTestBed } from '@angular/core/testing';
 import { Router } from '@angular/router';
+import { Message } from '../../model/message';
 
 @Component({
   selector: 'app-contact-form',
@@ -11,13 +12,12 @@ import { Router } from '@angular/router';
 export class ContactFormComponent implements OnInit {
   @ViewChild('messageTextarea') messageTextarea: ElementRef | undefined;
 
-  messageEl = {
-    nameNadSurname: '',
-    companyName: '',
-    email: '',
-    phoneNo: '',
-    message: '',
-  };
+  messageEl: Message = new Message();
+  nameAndSurnameNotValid = false;
+  emailNotValid = false;
+  phoneNoNotValid = false;
+  messageNotValid = false;
+
   scrollHeight: number | undefined;
   formGroup: FormGroup;
   textareaLength = '0';
@@ -46,7 +46,25 @@ export class ContactFormComponent implements OnInit {
   get message() {
     return this.formGroup.get('message');
   }
-
+  submitForm() {
+    if (this.formGroup.valid) {
+      this.messageEl.nameNadSurname = this.nameAndSurname?.value;
+      this.messageEl.companyName = this.companyName?.value;
+      this.messageEl.email = this.email?.value;
+      this.messageEl.phoneNo = this.phoneNo?.value;
+      this.messageEl.message = this.message?.value;
+    } else {
+      if (!this.nameAndSurname?.valid) this.nameAndSurnameNotValid = true;
+      else this.nameAndSurnameNotValid = false;
+      if (!this.email?.valid) this.emailNotValid = true;
+      else this.emailNotValid = false;
+      if (!this.phoneNo?.valid) this.phoneNoNotValid = true;
+      else this.phoneNoNotValid = false;
+      if (!this.message?.valid) this.messageNotValid = true;
+      else this.messageNotValid = false;
+      console.log(this.email?.valid);
+    }
+  }
   recountTextareaAmount(event: Event) {
     let elementHeight = this.messageTextarea?.nativeElement.offsetHeight;
     this.scrollHeight = this.messageTextarea?.nativeElement.scrollHeight;
